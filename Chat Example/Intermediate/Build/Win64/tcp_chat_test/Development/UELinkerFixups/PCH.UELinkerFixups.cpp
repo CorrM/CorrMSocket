a56736e81f0c539fbbc420abc92ca9b4d7ad9d0d
Islam Nofl <islamnofl.official@gmail.com>
2018-10-02 14:07:23 +0200

    go to v2

--- Chat Example/Intermediate/Build/Win64/tcp_chat_test/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/CorrM/Documents/Unreal Projects/tcp_chat_test/Intermediate/Build/Win64/tcp_chat_test/Development/UELinkerFixups/PCH.UELinkerFixups.h"
--- Chat Example/Intermediate/Build/Win64/tcp_chat_test/Development/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for C:\Users\CorrM\Documents\Unreal Projects\tcp_chat_test\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
-#include "C:\Users\CorrM\Documents\Unreal Projects\tcp_chat_test\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
