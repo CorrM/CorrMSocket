aa56a2ce71b36c51c69b1623df696b6299cf4bcb
Islam Nofl <islamnofl.official@gmail.com>
2018-10-02 14:07:46 +0200

    go to v2

--- Puzzle Example/Intermediate/Build/Win64/Puzzle_Test/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/CorrM/Documents/Unreal Projects/Puzzle_Test/Intermediate/Build/Win64/Puzzle_Test/Development/UELinkerFixups/PCH.UELinkerFixups.h"
--- Puzzle Example/Intermediate/Build/Win64/Puzzle_Test/Development/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for C:\Users\CorrM\Documents\Unreal Projects\Puzzle_Test\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
-#include "C:\Users\CorrM\Documents\Unreal Projects\Puzzle_Test\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
